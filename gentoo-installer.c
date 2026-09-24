503d82b70d4eccc7df94847b1473cb63309652d0
Gh0st-8221 <darkr.root.system@proton.me>
2026-09-24 15:25:11 +0300

    update backup from

--- gentoo-installer.c
+++ gentoo-installer.c
@@ -257,6 +257,7 @@ int main(void) {
     fprintf(script, "mkdir -p /etc/portage/package.use\n");
     fprintf(script, "echo 'net-wireless/wpa_supplicant dbus' > /etc/portage/package.use/wpa_supplicant\n");
     fprintf(script, "echo 'sys-kernel/installkernel dracut grub' > /etc/portage/package.use/installkernel\n");
+    fprintf(script, "echo 'media-libs/libglvnd X' > /etc/portage/package.use/libglvnd\n");
 
     fprintf(script, "emerge-webrsync\n");
 
